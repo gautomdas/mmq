4b0ed95e7812e1ef71c24c07011193d2d5c93116
Gautom Das <gautomdas4@gmail.com>
2024-07-08 13:02:38 -0400

    Add pycocoevalcap as a submodule

---
+++ README.md
@@ -0,0 +1,1 @@
+# blip2-coco
---
+++ datasets/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,6 @@
+from .vqa import VQA
+from .flickr30k import Flickr30k
+from .coco import COCODataset
+
+__all__ = ['Flickr30k', 'COCODataset', 'VQA']
+# __all__ = ['Flickr30k', 'COCODataset']
