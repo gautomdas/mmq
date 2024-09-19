c4eb04a4690e12788a24bfc1b0b5a331a01696da
Vincent La <vincentla20877@gmail.com>
2024-09-19 17:54:45 -0400

    start formalizing awq, split out to appropriate files

---
+++ awq/scaled_modules.py
@@ -0,0 +1,14 @@
+# custom pytorch modules to apply AWQ weight scaling before forward pass of another module
+import torch.nn as nn
+
+class ScaledModule(nn.Module):
+
+    def __init__(self, scales, module):
+        super().__init__()
+        self.scales = nn.Parameter(scales.data)
+        self.module = module
+
+    def forward(self, x):
+        # TODO: check shape here
+        x = x / self.scales.view(1, 1, -1).to(x.device)
+        return self.module(x)
